1b2e553680b46030f37143af7a5da223d1de6bf7
X-Liang <1129584401@qq.com>
2020-04-24 22:59:59 +0800

    #update #Java 提取操作符 Token

--- TinyScript-Java/src/main/java/lexer/Token.java
+++ TinyScript-Java/src/main/java/lexer/Token.java
@@ -27,6 +27,7 @@ public class Token {
 
     /**
      * 提取变量或者关键字，或者时 true，false
+     *
      * @param it 字符流
      * @return
      */
@@ -57,20 +58,19 @@ public class Token {
 
     /**
      * 使用状态机，提取字符串的值
+     *
      * @param it 字符串流，以" 开头或者' 开头
-     * @return
-     *
-     * state 初始为 0
-     *  0 -> 1  ：状态 0 接受了一个 " 字符
-     *  0 -> 2  ：状态 0 接受一个 ' 字符
-     *
-     *  1 -> 1 : 状态 1 接受一个非“ 的字符
-     *  1 -> str: 状态 1 接受一个 " 字符
-     *
-     *  2 -> 2 : 状态 2 接受一个非' 的字符
-     *  2 -> str : 状态 1 接受一个 ' 字符
+     * @return state 初始为 0
+     * 0 -> 1  ：状态 0 接受了一个 " 字符
+     * 0 -> 2  ：状态 0 接受一个 ' 字符
+     * <p>
+     * 1 -> 1 : 状态 1 接受一个非“ 的字符
+     * 1 -> str: 状态 1 接受一个 " 字符
+     * <p>
+     * 2 -> 2 : 状态 2 接受一个非' 的字符
+     * 2 -> str : 状态 1 接受一个 ' 字符
      */
-    public static Token makeString(PeekIterator<Character> it) throws LexicalException{
+    public static Token makeString(PeekIterator<Character> it) throws LexicalException {
         StringBuilder tokenValue = new StringBuilder();
         int state = 0;
         while (it.hasNext()) {
@@ -102,6 +102,110 @@ public class Token {
             }
         }
         throw new LexicalException("Exception Error");
+    }
+
+
+    public static Token makeOperator(PeekIterator<Character> it) throws LexicalException {
+        StringBuilder operator = new StringBuilder();
+        int state = 0;
+        while (it.hasNext()) {
+            Character currentChar = it.next();
+            operator.append(currentChar);
+            switch (state) {
+                case 0: {
+                    if (currentChar == ',' || currentChar == ';') {
+                        return new Token(TokenType.OPERATOR, operator.toString());
+                    } else if (currentChar == '+') {
+                        state = 1;
+                    } else if (currentChar == '-') {
+                        state = 2;
+                    } else if (currentChar == '*') {
+                        state = 3;
+                    } else if (currentChar == '/') {
+                        state = 4;
+                    } else if (currentChar == '>') {
+                        state = 5;
+                    } else if (currentChar == '<') {
+                        state = 6;
+                    } else if (currentChar == '=') {
+                        state = 7;
+                    } else if (currentChar == '!') {
+                        state = 8;
+                    } else if (currentChar == '&') {
+                        state = 9;
+                    } else if (currentChar == '|') {
+                        state = 10;
+                    } else if (currentChar == '^') {
+                        state = 11;
+                    } else if (currentChar == '%') {
+                        state = 12;
+                    }
+                    break;
+                }
+                case 1: {
+                    if (!(currentChar == '+' || currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// ++  += +
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }
+                case 2: {
+                    if (!(currentChar == '-' || currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// --  -= -
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }
+                case 3:
+                case 4:
+                case 7:
+                case 8:
+                case 11:
+                case 12: {
+                    if (!(currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// * *=  / /= == != ^ ^= % %=
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }
+                case 5: {
+                    if (!(currentChar == '>' || currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// >> >= >
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }
+                case 6: {
+                    if (!(currentChar == '<' || currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// << <= <
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }/// ++  += +
+                case 9: {
+                    if (!(currentChar == '&' || currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// && &= &
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }
+                case 10: {
+                    if (!(currentChar == '|' || currentChar == '=')) {
+                        operator.deleteCharAt(operator.length() - 1);
+                        it.putBack();
+                    }
+                    /// |= | ||
+                    return new Token(TokenType.OPERATOR, operator.toString());
+                }
+            }
+        }
+        throw new LexicalException("Exception");
     }
 
     public String getValue() {
--- TinyScript-Java/src/test/java/lexer/TokenTests.java
+++ TinyScript-Java/src/test/java/lexer/TokenTests.java
@@ -3,7 +3,7 @@ package lexer;
 import common.PeekIterator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TokenTests {
     void assertToken(Token token, String exceptTokenValue, TokenType exceptTokenType) {
@@ -42,5 +42,57 @@ public class TokenTests {
             Token token = Token.makeString(it);
             assertToken(token, test, TokenType.STRING);
         }
+    }
+
+    @Test
+    public void test_makeOperator() throws LexicalException {
+        var it = new PeekIterator<Character>("+=".chars().mapToObj(c -> (char)c));
+        Token token = Token.makeOperator(it);
+        assertToken(token, "+=", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("+abc".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('a', it.next());
+        assertToken(token, "+", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("+ xxx".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals(' ', it.next());
+        assertToken(token, "+", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("++mm".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('m', it.next());
+        assertToken(token, "++", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("/=g".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('g', it.next());
+        assertToken(token, "/=", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("&=888".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('8', it.next());
+        assertToken(token, "&=", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("&777".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('7', it.next());
+        assertToken(token, "&", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("||xx".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('x', it.next());
+        assertToken(token, "||", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("^=1".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('1', it.next());
+        assertToken(token, "^=", TokenType.OPERATOR);
+
+        it = new PeekIterator<Character>("%7".chars().mapToObj(c -> (char)c));
+        token = Token.makeOperator(it);
+        assertEquals('7', it.next());
+        assertToken(token, "%", TokenType.OPERATOR);
     }
 }
